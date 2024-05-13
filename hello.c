7309edb32333ec0db29c80286ef6515b0266a3b3
rushi-1525 <rushisatani152530@gmail.com>
2024-05-13 09:20:04 +0530

    first commrnt

---
+++ hello.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+   
+int main(){
+
+// anser-1
+
+ int c;
+printf("enter c");
+scanf("%d", & c);
+int f=(c*9/5)+32;
+printf("youer ans:- %d",f);
+
+
+// anser-2
+// int a;
+// int b;
+// int c;
+// int d;
+// printf("Base Salary =");
+// scanf("%d",&a);
+
+// scanf("%d",&b);
+// int f=a*b/100;
+// printf("HAR ANS %d",f);
+// printf("DA =");
+// scanf("%d",&c);
+// int g=a*c/100;
+// printf("TA =");
+// scanf("%d",&d);
+// int e=a*d/100;
+// int ans= f+e+g;
+// printf("ANSER IS =%d",ans);
+
+// anser-3
+
+// int a;
+// int b;
+// printf("first angle:-");
+// scanf("%d",&a);
+
+// printf("second angle:-");
+// scanf("%d",&b);
+// int ans=180-a-b;
+// printf("ans:-%d",ans);
+
+// int a,b,c;
+// scanf("%d",&a);
+// scanf("%d",&b);
+// scanf("%d",&c);
+// if (a>b)
+// {
+//    printf("A is big");
+// }
+// else if(b>=a&&c){
+//     printf("b is big");
+// }
+//  if(c>=a&&b){
+//     printf("c is big");
+// }
+// else{
+//     printf("not valid number");
+// }
+
+
+// int year;
+// printf("enter year \n");
+// scanf("%d",&year);
+// if(year%4==0){
+// if(year%100==0){
+// if(year%400==0){
+// printf("leap year");
+// }else{
+//     printf("not a leap year");
+// }
+// }else{
+//     printf("leap year");
+// }
+// }else{
+// printf("not a leap year\n");
+// 
+
+// int income;
+
+// int b=10001<=20000;
+// int b=20001<=30000;
+// printf("enter youer income");
+// scanf("%d",income);
+
+// if (0<=10000)
+// {
+// printf("youer text is %d",income/5*100);
+
+// }
+// else if(10000<=10000){
+
+// }
+
+    return 0;
+}
